a3fa1ad8dab3b8cfe43ad65e58af08b2feb5e05a
Abdullahi Damisa <abdamsabinfo@gmail.com>
2022-01-24 17:47:06 +0000

    Cocktail

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section" >
+      <h1 className="section-title" >about us</h1>
+
+			<p>
+				Agreed joy vanity regret met may ladies oppose who. Mile fail as left as hard eyes. 
+				Meet made call in mean four year it to. Prospect so branched wondered sensible of up. 
+				For gay consisted resolving pronounce sportsman saw discovery not. Northward or household 
+				as conveying we earnestly believing. No in up contrasted discretion inhabiting excellence. 
+				Entreaties we collecting unpleasant at everything conviction.
+			</p>
+
+    </section>
   )
 }
 
