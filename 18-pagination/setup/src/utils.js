b2b154655050ae66c734d61b52ebe46bd3699913
Abdullahi Damisa <abdamsabinfo@gmail.com>
2022-01-27 18:18:43 +0000

    Pagination

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+	const itemPerPage = 9;
+	const pages = Math.ceil(followers.length/itemPerPage);
+
+	const newFollower = Array.from({length:pages}, (_, index)=>{
+		const start = index * itemPerPage
+		return followers.slice(start, start+itemPerPage)
+	})
+
+	return newFollower
+	
+}
 
 export default paginate
